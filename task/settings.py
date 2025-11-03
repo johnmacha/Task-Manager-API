cf8ddd1b8a7b4a2e1d0218f5e514e8a5548e5f41
johnmacha <jontehmacha@gmail.com>
2025-11-03 21:55:22 +0300

    Fixed JWT authentication and tested API access with Postman

--- task/settings.py
+++ task/settings.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
