1a471693bf0adfd64e3fbb11821af49df359d22a
johnmacha <jontehmacha@gmail.com>
2025-11-17 23:10:11 +0300

    fix(settings): set CORS_ALLOW_ORIGINS to False

--- task/settings.py
+++ task/settings.py
@@ -62,13 +62,14 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", #Vite default
     "http://127.0.0.1:5173", #Sometimes needed too
     "https://task-manager-frontend.onrender.com",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = False
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
