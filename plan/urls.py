c42f6127abdba61986587ab71291a1be5d91fe26
johnmacha <jontehmacha@gmail.com>
2025-11-18 15:31:33 +0300

    feat: add /api/login/ endpoint as alias for JWT token authentication

--- plan/urls.py
+++ plan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'plan', TaskViewSet, basename='plan')
@@ -8,4 +9,6 @@ router.register(r'plan', TaskViewSet, basename='plan')
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name = 'register'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
    ]
